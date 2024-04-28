e50811c02bfd712feb843768121edfcf386f41d4
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-04-28 16:34:46 +0530

    heroSection

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
 export const Hero = () =>{
     return (
-        <div>Hero</div>
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Lakshmi</h1>
+                <p className={styles.description}> I'm a full-stack developer. I love crafting interactive web experiences using MongoDB, Express.js, React.js, and Node.js. Let's turn your ideas into reality!</p>
+                <a href="mailto:lakshmishajan.me@gmail.com" className={styles.contactBtn}>Contact Me</a>
+            </div>
+            <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+        </section>
     );
 }
