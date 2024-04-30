533f326cd8b7e3e422359a8e6b6b2f48e26453b1
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-04-30 20:27:12 +0530

    about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,34 +1,43 @@
 import React from "react";
+
+import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 export const About = () =>{
-    return <section>
-        <h2>About</h2>
-        <div><img src={getImageUrl("about/aboutImage.png")} alt="me" />
-        <ul>
-            <li>
+    return (
+        <section className={styles.container} id="about">
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+        <img 
+        src={getImageUrl("about/aboutImage.png")} 
+        alt="me" 
+        className={styles.aboutImage} 
+        />
+        <ul className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
                 <img src={getImageUrl("about/cursorIcon.png")} alt="cursoricon" />
-                 <div>
+                 <div className={styles.aboutItemText}>
                     <h3>Frontend Developer</h3>
                     <p>I'm a frontend developer with experience in building responsive and optimized sites</p>
                  </div>
         </li>
-        <li>
+        <li className={styles.aboutItem}>
                 <img src={getImageUrl("about/serverIcon.png")} alt="servericon" />
-                 <div>
+                 <div  className={styles.aboutItemText}>
                     <h3>Backend Developer</h3>
                     <p>I have experience developing fast and optimised back-end systems and APIs</p>
                  </div>
         </li>
-        <li>
-                <img src={getImageUrl("about/serverIcon.png")} alt="cursoricon" />
-                 <div>
+        <li className={styles.aboutItem}>
+                <img src={getImageUrl("about/cursorIcon.png")} alt="cursoricon" />
+                 <div  className={styles.aboutItemText}>
                     <h3>UI Designer</h3>
-                    <p> I'm currently enhancing my proficiency in design software such as Adobe XD and Figma through hands-on practice and online tutorials.
+                    <p> I'm currently enhancing my proficiency in design software such as Adobe photoshop and Figma through hands-on practice and online tutorials.
                    </p>
                  </div>
         </li>
         </ul>
         </div>
     </section>
+    );
 }
